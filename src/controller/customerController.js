ad0db655e98d9ede8d0239d8e8f34a0bb67a3144
RamdlanFaqih <ramfaqih@gmail.com>
2023-09-25 18:44:12 +0700

    add customer

---
+++ src/controller/customerController.js
@@ -0,0 +1,203 @@
+const {
+  getCustomerM,
+  selectPagination,
+  pagination,
+  getCustomerId,
+  getCustomerEmail,
+  createCustomerM,
+  loginCustomerM,
+  updateCustomerM,
+  deleteCustomerM,
+} = require("../model/customerModel");
+
+const { generateToken, refreshToken } = require("../helper/jwt");
+const bcrypt = require("bcrypt");
+const cloudinary = require("../config/cloudinaryConfig");
+
+const customerController = {
+  getCustomer: async (req, res) => {
+    let search = req.query.search || "";
+    let sort = req.query.sort || "ASC";
+    try {
+      let result = await getCustomerM(search, sort);
+      console.log(result);
+      res.status(200).json({
+        message: "get customer successfully",
+        data: result.rows,
+      });
+    } catch (err) {
+      res.status(400).json({
+        error: err.message,
+        message: "error getting customer ",
+      });
+    }
+  },
+
+  selectPage: async (req, res) => {
+    let { limit, page } = req.query;
+    let pageValue = page ? Number(page) : 1;
+    let limitValue = limit ? Number(limit) : 2;
+    const offsetValue = pageValue === 1 ? 0 : (pageValue - 1) * limitValue;
+    const allData = await selectPagination();
+    const totalData = Number(allData.rows[0].total);
+    try {
+      let result = await pagination(limitValue, offsetValue);
+      res.status(200).json({
+        message: "user has been selected by limit and offset",
+        currentPage: pageValue,
+        dataPerPage: limitValue,
+        totalPage: Math.ceil(totalData / limitValue),
+        totalData,
+        data: result.rows,
+      });
+    } catch (err) {
+      res.status(400).json({
+        error: err.message,
+      });
+    }
+  },
+
+  getCustomerById: async (req, res) => {
+    try {
+      const customer_id = req.params.customer_id;
+      const result = await getCustomerId(customer_id);
+      res.json({
+        data: result.rows[0],
+        message: "get data successfully",
+      });
+    } catch (err) {
+      res.json({
+        error: err.message,
+        message: "error getting user",
+      });
+    }
+  },
+  createCustomer: async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+
+      let { rowCount } = await getCustomerEmail(email);
+      if (rowCount) {
+        return res
+          .status(400)
+          .json({ message: "email already in use,please use another email" });
+      }
+      bcrypt.hash(password, 10, async (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            message: "Error hashing password",
+            error: err.message,
+          });
+        }
+        const user = {
+          name,
+          email,
+          password: hash,
+        };
+        console.log(user);
+
+        try {
+          const userData = await createCustomerM(user);
+          res.status(200).json({
+            message: "Customer has been created successfully",
+            data: userData,
+          });
+        } catch (err) {
+          console.error("Error creating Customer:", err);
+          res.status(400).json({
+            message: "Error creating Customer",
+            err: err.message,
+          });
+        }
+      });
+    } catch (err) {
+      res.status(400).json({
+        message: "Error creating customer Catch",
+        err: err.message,
+      });
+    }
+  },
+
+  loginCustomer: async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      const result = await loginCustomerM(email);
+
+      if (result.rowCount > 0) {
+        const passwordHash = result.rows[0].password;
+        const PasswordValid = await bcrypt.compare(password, passwordHash);
+        const user = result.rows[0];
+
+        if (PasswordValid) {
+          const token = await generateToken({
+            customer: user,
+          });
+
+          return res.status(200).json({
+            message: "Login successful",
+            token: token,
+            data: user,
+          });
+        } else {
+          res.status(400).json({ message: "Invalid email or password " });
+        }
+      } else {
+        res.status(400).json({ message: "Invalid email or password " });
+      }
+    } catch (error) {
+      res.status(400).json({ error, message: "error during login" });
+    }
+  },
+
+  updateCustomer: async (req, res) => {
+    try {
+      const customer_id = req.params.customer_id;
+      // console.log(req);
+      const customerImage = await cloudinary.uploader.upload(req.file.path, {
+        folder: "customer",
+      });
+      const result = await getCustomerId(Number(customer_id));
+      console.log(result);
+      const user = result.rows[0];
+      const data = {
+        name: req.body.name ?? user.name,
+        email: req.body.email ?? user.email,
+        phone_number: req.body.phone_number ?? user.phone_number,
+        gender: req.body.gender ?? user.gender,
+        birthday: req.body.birthday ?? user.birthday,
+        image: customerImage.secure_url ?? user.image,
+      };
+
+      await updateCustomerM(data, Number(customer_id));
+
+      res.status(200).json({
+        message: "Update Successfull",
+      });
+    } catch (error) {
+      res.status(400).json({
+        message: "Update Error",
+        error: error.message,
+      });
+    }
+  },
+
+  deleteCustomer: async (req, res) => {
+    try {
+      const customer_id = req.params.customer_id;
+      const result = await deleteCustomerM(customer_id);
+      const data = await cloudinary.uploader.destroy(result);
+      res.json({
+        message: "delete data sucessfully",
+        data: `id ${data} has been deleted`,
+      });
+    } catch (err) {
+      res.json({
+        error: err.message,
+        message: "error deleting data",
+      });
+    }
+  },
+};
+
+module.exports = customerController;
---
+++ src/model/customerModel.js
@@ -0,0 +1,93 @@
+const db = require("../config/db");
+
+const getCustomerM = (search, sort) => {
+  return db.query(`
+          SELECT * 
+          FROM customer
+          WHERE customer.name LIKE '%${search}%'
+          ORDER BY customer.name ${sort}
+      `);
+};
+
+const selectPagination = () => {
+  return db.query("SELECT COUNT(*) AS total FROM customer");
+};
+
+const pagination = (limit, offset) => {
+  return db.query(`SELECT *FROM customer LIMIT ${limit} OFFSET ${offset}`);
+};
+
+const getCustomerId = (customer_id) => {
+  return db.query(
+    `SELECT * FROM customer WHERE customer.customer_id=${customer_id}`
+  );
+};
+
+const getCustomerEmail = (email) => {
+  return new Promise((resolve, reject) =>
+    db.query(
+      `SELECT * FROM customer WHERE customer.email = '${email}'`,
+      (err, res) => {
+        if (!err) {
+          resolve(res);
+        } else {
+          reject(err.message);
+        }
+      }
+    )
+  );
+};
+
+const createCustomerM = (data) => {
+  const { name, email, password } = data;
+  const role = "customer";
+  return new Promise((resolve, reject) =>
+    db.query(
+      `INSERT INTO customer(name, email, password, role) VALUES('${name}', '${email}', '${password}', '${role}')`,
+      (err, res) => {
+        if (!err) {
+          resolve(res);
+        } else {
+          reject(err.message);
+        }
+      }
+    )
+  );
+};
+
+const loginCustomerM = (email) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `SELECT * FROM customer WHERE customer = '${email}'`,
+      (err, res) => {
+        if (err) return reject(err);
+        resolve(res);
+      }
+    );
+  });
+};
+
+const updateCustomerM = (data, customer_id) => {
+  const { name, email, phone_number, gender, birthday, image } = data;
+  return db.query(
+    `UPDATE customer SET name='${name}', email='${email}', phone_number='${phone_number}', gender='${gender}', birthday='${birthday}', image='${image}' WHERE customer.customer_id = ${customer_id}`
+  );
+};
+
+const deleteCustomerM = (customer_id) => {
+  return db.query(
+    `DELETE FROM customer WHERE customer.customer_id=${customer_id}`
+  );
+};
+
+module.exports = {
+  getCustomerM,
+  selectPagination,
+  pagination,
+  getCustomerId,
+  getCustomerEmail,
+  createCustomerM,
+  loginCustomerM,
+  updateCustomerM,
+  deleteCustomerM,
+};
